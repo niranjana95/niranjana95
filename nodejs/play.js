1be440df56e5898cf58d07fc2252536b6951e0bc
niranjana95 <72014525+niranjana95@users.noreply.github.com>
2024-03-13 18:18:51 +0530

    Add files via upload

--- nodejs/play.js
+++ nodejs/play.js
@@ -1,14 +1,6 @@
-const multiply=(num1,num2)=>num1*num2;
+const http=require('http');
+const server=http.createServer((req,res)=>{
+console.log(req);
+});
+server.listen(4000);
 
-const students={
-    firstName:"niranjana",
-    lastName:"Balamurugan",
-    age:20,
-    grade:"A",
-    getInfo:function(){
-        return `${this.firstName} ${this.lastName}, Age: ${this.age}, Grade: ${this.grade}`;
-    }
-
-};
-console.log(multiply(5,10));
-console.log(students.getInfo());
